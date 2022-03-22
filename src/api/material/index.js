4974f94221a0be8f7a1f2d949422117220d7d4b4
heptaluan <vd2o@foxmail.com>
2022-03-22 22:36:16 +0800

    add

--- src/api/material/index.js
+++ src/api/material/index.js
@@ -1,28 +1,28 @@
 import { getAction, postAction, putAction, deleteAction } from '@/api/manage'
 
 // 新增出库模版
-export const addMaterialList = params => postAction('/mission/customMaterialPlan/add', params)
+export const addMaterialList = (params) => postAction('/mission/customMaterialPlan/add', params)
 
 // 通过 id 查询
-export const queryMaterialPlan = params => getAction('mission/customMaterialPlan/queryById', params)
+export const queryMaterialPlan = (params) => getAction('mission/customMaterialPlan/queryById', params)
 
 // 通过 id 删除
-export const delMaterialPlan = params => deleteAction('mission/customMaterialPlan/delete', params)
+export const delMaterialPlan = (params) => deleteAction('mission/customMaterialPlan/delete', params)
 
 // 编辑/更新
-export const updateMaterialPlan = params => putAction('mission/customMaterialPlan/edit', params)
+export const updateMaterialPlan = (params) => putAction('mission/customMaterialPlan/edit', params)
 
 // 获取模版列表
-export const getMaterialPlanList = params => getAction('mission/customMaterialPlan/list', params)
+export const getMaterialPlanList = (params) => getAction('mission/customMaterialPlan/list', params)
 
 // 获取地址列表
-export const getAddressList = params => getAction('tailai-system/mission/addressInfo/list', params)
+export const getAddressList = (params) => getAction('tailai-system/mission/addressInfo/list', params)
 
 // 查询销售人员列表
-export const queryRoleUsers = params => getAction('/tailai-system/sys/user/queryRoleUsers', params)
+export const queryRoleUsers = (params) => getAction('/tailai-system/sys/user/queryRoleUsers', params)
 
 // 查询病例编号
-export const queryCaseCode = params => getAction('/tailai-system/mission/caseCode/list', params)
+export const queryCaseCode = (params) => getAction('/tailai-system/mission/caseCode/list', params)
 
 // export const getCatalogDepart = params => getAction('/sys/sysDepart/getCatalogDepart', params)
 // export const queryByOrgCodeForAddressList = params => getAction('/sys/user/queryByOrgCodeForAddressList', params)
--- src/api/order/index.js
+++ src/api/order/index.js
@@ -7,79 +7,82 @@
 import { getAction, postAction, putAction, uploadAction } from '@/api/manage'
 import { axios } from '@/utils/request'
 
-export const queryProductOrderList = params => getAction('/multiomics/productOrder/list', params)
-export const getCatalogDepart = params => getAction('/sys/sysDepart/getCatalogDepart', params)
-export const queryByOrgCodeForAddressList = params => getAction('/sys/user/queryByOrgCodeForAddressList', params)
-export const getOrderStepInfo = params => getAction('/multiomics/productOrder/orderStepInfo', params)
-export const getOrderInfoById = params => getAction('/multiomics/productOrder/queryById', params)
-export const filishOrder = params => postAction('/multiomics/productOrder/createOrderInstance', params)
+export const queryProductOrderList = (params) => getAction('/multiomics/productOrder/list', params)
+export const getCatalogDepart = (params) => getAction('/sys/sysDepart/getCatalogDepart', params)
+export const queryByOrgCodeForAddressList = (params) => getAction('/sys/user/queryByOrgCodeForAddressList', params)
+export const getOrderStepInfo = (params) => getAction('/multiomics/productOrder/orderStepInfo', params)
+export const getOrderInfoById = (params) => getAction('/multiomics/productOrder/queryById', params)
+export const filishOrder = (params) => postAction('/multiomics/productOrder/createOrderInstance', params)
 export const addOrderMedicalCase = (params, orderId) =>
   postAction('/multiomics/productOrder/orderMedicalCase/' + orderId, params)
-export const addProductOrder = params => postAction('/multiomics/productOrder/add', params)
-export const putProductOrder = params => putAction('/multiomics/productOrder/edit', params)
+export const addProductOrder = (params) => postAction('/multiomics/productOrder/add', params)
+export const putProductOrder = (params) => putAction('/multiomics/productOrder/edit', params)
 export const putOrderClinicalInfo = (orderId, params) =>
   postAction('multiomics/productOrder/orderClinicalInfo/' + orderId, params)
 
 // 通过身份证号去寻找或者创建用户
-export const lookUpMedicalCase = params =>
+export const lookUpMedicalCase = (params) =>
   getAction('/tailai-multiomics/multiomics/medicalCase/lookUpMedicalCase', params)
 
 // 添加申请人信息
-export const createOrderApplyInfo = params => postAction('/multiomics/productOrder/createOrder/applyInfo', params)
+export const createOrderApplyInfo = (params) => postAction('/multiomics/productOrder/createOrder/applyInfo', params)
 
 // 创建订单-添加检测项目
-export const createOrderDetectInfo = params => postAction('multiomics/productOrder/createOrder/detectInfo', params)
+export const createOrderDetectInfo = (params) => postAction('multiomics/productOrder/createOrder/detectInfo', params)
 
 // 创建订单-添加临床信息
-export const createOrderClinicalInfo = params =>
+export const createOrderClinicalInfo = (params) =>
   postAction('/tailai-multiomics/multiomics/productOrder/createOrder/clinicalInfo', params)
 
 // 产品分组查询
-export const getProductInfoList = params => getAction('/tailai-system/mission/productInfo/listByGroup', params)
+export const getProductInfoList = (params) => getAction('/tailai-system/mission/productInfo/listByGroup', params)
 
 // 订单实例创建（完成）
-export const createOrderInstance = orderId =>
+export const createOrderInstance = (orderId) =>
   postAction(`/tailai-multiomics/multiomics/productOrder/createOrderInstance/${orderId}`)
 
 // 查询订单所有实例
-export const getProductOrderInstanceList = id => getAction(`/tailai-multiomics/multiomics/productOrderInstance/history/${id}`)
+export const getProductOrderInstanceList = (id) =>
+  getAction(`/tailai-multiomics/multiomics/productOrderInstance/history/${id}`)
 
 // 查询订单进度和子订单
 // export const getProductOrderInstance = id => getAction(`/tailai-multiomics/multiomics/productOrderInstance/info/${id}`)
-export const getProductOrderInstance = params => getAction(`/tailai-multiomics/multiomics/productOrderInstance/info`, params)
+export const getProductOrderInstance = (params) =>
+  getAction(`/tailai-multiomics/multiomics/productOrderInstance/info`, params)
 
 // 订单实例重检
-export const reDetectInstance = params => postAction('/tailai-multiomics/multiomics/productOrder/reDetectInstance', params)
+export const reDetectInstance = (params) =>
+  postAction('/tailai-multiomics/multiomics/productOrder/reDetectInstance', params)
 
 // 查询订单进度和子订单
 export function deleteTempleOrder(orderId) {
   return axios({
     url: `/tailai-multiomics/multiomics/productOrder/deleteTempleOrder/${orderId}`,
-    method: 'delete'
+    method: 'delete',
   })
 }
 
 // 订单样本物流信息
-export const getOrderSampleInfo = id => getAction(`/tailai-multiomics/multiomics/productOrder/orderSampleInfo/${id}`)
+export const getOrderSampleInfo = (id) => getAction(`/tailai-multiomics/multiomics/productOrder/orderSampleInfo/${id}`)
 
 // 文件上传
-export const CommonSingleUpload = params => uploadAction('mission/fileInfo/commonSingleUpload', params)
+export const CommonSingleUpload = (params) => uploadAction('mission/fileInfo/commonSingleUpload', params)
 
 // 执行批量任务
-export const startOrderImportTask = params => postAction('multiomics/orderImportTask/add', params)
+export const startOrderImportTask = (params) => postAction('multiomics/orderImportTask/add', params)
 
 // 批量任务查询
-export const getOrderImportTask = params => getAction('multiomics/orderImportTask/list', params)
+export const getOrderImportTask = (params) => getAction('multiomics/orderImportTask/list', params)
 
 // 订单列表查询
-export const getproductOrderOrderUserView = params => getAction('multiomics/productOrder/orderUserView', params)
+export const getproductOrderOrderUserView = (params) => getAction('multiomics/productOrder/orderUserView', params)
 
 // 历史编号导入
-export const uploadHistoryCaseBarcode = params => postAction('/mission/codeManagement/uploadHistoryCaseBarcode', params)
+export const uploadHistoryCaseBarcode = (params) =>
+  postAction('/mission/codeManagement/uploadHistoryCaseBarcode', params)
 
 // 病例列表
-export const getMedicalCaseList = params => getAction('multiomics/medicalCase/list', params)
-
+export const getMedicalCaseList = (params) => getAction('multiomics/medicalCase/list', params)
 
 // 快速订单
 // export function getUserInfo(params) {
@@ -89,13 +92,13 @@ export const getMedicalCaseList = params => getAction('multiomics/medicalCase/li
 //     params
 //   })
 // }
-export const getUserInfo = params => getAction('multiomics/medicalCase/lookUpMedicalCase', params)
+export const getUserInfo = (params) => getAction('multiomics/medicalCase/lookUpMedicalCase', params)
 
 export function getDistributor(params) {
   return axios({
     url: `mission/businessAccess/mobileFuzzyAccess`,
     method: 'get',
-    params
+    params,
   })
 }
 
@@ -103,7 +106,7 @@ export function getHospital(params) {
   return axios({
     url: `mission/businessAccess/mobileFuzzyHospital`,
     method: 'get',
-    params
+    params,
   })
 }
 
@@ -111,12 +114,13 @@ export function getSeller(params) {
   return axios({
     url: `sys/user/mobileFuzzySeller`,
     method: 'get',
-    params
+    params,
   })
 }
 
-export const checkBarcode = params => getAction('/mission/caseCode/mobileValidBarcode', params)
-export const addOrder = params => postAction('multiomics/productOrder/mBookingOrder', params)
-export const getOrderSingleInfo = params => getAction('/multiomics/productOrder/mOrderBookingInfo', params)
-export const editSingleOrder = (orderId, params) => putAction(`/multiomics/productOrder/editOrderBookingInfo/${orderId}`, params)
-export const auditOrder = params => postAction('/multiomics/productOrder/auditOrder', params)
+export const checkBarcode = (params) => getAction('/mission/caseCode/mobileValidBarcode', params)
+export const addOrder = (params) => postAction('multiomics/productOrder/mBookingOrder', params)
+export const getOrderSingleInfo = (params) => getAction('/multiomics/productOrder/mOrderBookingInfo', params)
+export const editSingleOrder = (orderId, params) =>
+  putAction(`/multiomics/productOrder/editOrderBookingInfo/${orderId}`, params)
+export const auditOrder = (params) => postAction('/multiomics/productOrder/auditOrder', params)
