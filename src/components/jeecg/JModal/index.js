2a0bad5a5deaa0dec1fbe8d7370461e193d13542
heptaluan <vd2o@foxmail.com>
2022-03-28 23:48:56 +0800

    format

--- src/components/jeecg/JModal/index.js
+++ src/components/jeecg/JModal/index.js
@@ -4,13 +4,10 @@ import JPrompt from './JPrompt'
 export default {
   install(Vue) {
     Vue.component(JModal.name, JModal)
-
     const JPromptExtend = Vue.extend(JPrompt)
     Vue.prototype.$JPrompt = function (options = {}) {
-      // 创建prompt实例
       const vm = new JPromptExtend().$mount()
       vm.show(options)
-      // 关闭后销毁
       vm.$on('after-close', () => vm.$destroy())
       return vm
     }
